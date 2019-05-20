3942d53ad23cf5fe724a9db3733fde65438d2fbe
Noah Blon <nblon@nerdery.com>
2019-05-20 17:28:50 -0500

    feat: add images processed through gatsby image to posts
    add images to yaml frontmatter and consume via graphql. pass through react component to use gatsby
    image component and output in mdx.

--- src/hooks/use-posts.js
+++ src/hooks/use-posts.js
@@ -9,18 +9,39 @@ export default function usePosts() {
             title
             slug
             date
+            images {
+              publicURL
+              childImageSharp {
+                fluid {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  sizes
+                  originalImg
+                  originalName
+                  presentationWidth
+                  presentationHeight
+                }
+              }
+            }
           }
           excerpt
         }
       }
     }
   `;
+
   const data = useStaticQuery(query);
   return data.allMdx.nodes.map(post => {
     return {
       title: post.frontmatter.title,
       slug: post.frontmatter.slug,
       date: post.frontmatter.date,
+      images: post.frontmatter.images,
       excerpt: post.excerpt
     };
   });
--- src/templates/post.js
+++ src/templates/post.js
@@ -2,12 +2,33 @@ import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-mdx";
 import Layout from "../components/layout";
+import Img from "gatsby-image";
+import PropTypes from "prop-types";
 
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        images {
+          publicURL
+          childImageSharp {
+            fluid {
+              base64
+              tracedSVG
+              aspectRatio
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              sizes
+              originalImg
+              originalName
+              presentationWidth
+              presentationHeight
+            }
+          }
+        }
       }
       code {
         body
@@ -16,11 +37,36 @@ export const query = graphql`
   }
 `;
 
-const PostTemplate = ({ data: { mdx: post } }) => (
-  <Layout>
-    <h1>{post.frontmatter.title}</h1>
-    <MDXRenderer>{post.code.body}</MDXRenderer>
-  </Layout>
-);
+function buildImageVm(frontmatterImages) {
+  const images = {};
+  if (frontmatterImages) {
+    frontmatterImages.forEach((image, i) => {
+      const { publicURL } = image;
+      const { childImageSharp } = image;
+      const GatsbyImage = () => <Img fluid={childImageSharp.fluid} />;
+      const nativeImage = () => <img src={publicURL} />;
+      if (childImageSharp !== null) {
+        images[`image${i}`] = GatsbyImage;
+      } else {
+        images[`image${i}`] = nativeImage;
+      }
+    });
+  }
+  return images;
+}
+
+const PostTemplate = ({ data: { mdx: post } }) => {
+  const images = buildImageVm(post.frontmatter.images);
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <MDXRenderer images={images}>{post.code.body}</MDXRenderer>
+    </Layout>
+  );
+};
+
+PostTemplate.propTypes = {
+  data: PropTypes.object
+};
 
 export default PostTemplate;
